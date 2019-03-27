8433f5e493ee1e469195356fbdb77011adfdf193
LeeCode <1063138668@qq.com>
2019-03-27 22:06:30 +0800

    add explanation

--- Day02.java
+++ Day02.java
@@ -2,6 +2,11 @@ public class Solution {
     /**
      * @param A: An integers array.
      * @return: return any of peek positions.
+	 solution:这个题目的主要关键点在于抓住 A[1] > A[0] 和 A[len-2] > A[len-1]这两个题目说明
+	 所以可以采用二分，当mid不满足条件的时候，判断A[mid-1]和A[mid]的关系 与 A[mid+1]和A[mid]的关系
+	 若A[mid+1] > A[mid]，则[mid + 1, r]中肯定存在答案满足条件，因为若后面是一个递增的情况，则答案就是倒数第二个元素(A[len-2] > A[len-1]);
+	 若后面不是一个递增的情况，即存在一个下标i， mid ... i递增,而A[i] > A[i+1]则答案就是i-1指向的元素
+	 A[mid-1] > A[mid]的情况类似讨论
      */
     static int n;
     public int findPeak(int[] A) {
